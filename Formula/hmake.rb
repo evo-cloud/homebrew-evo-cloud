147d4a3878259c43bbccecad5ad1e4297bed7b83
EaseWay Hu <easeway@gmail.com>
2016-05-18 01:24:02 -0700

    upgrade hmake to v1.0.0rc3

--- Formula/hmake.rb
+++ Formula/hmake.rb
@@ -1,9 +1,9 @@
 class  Hmake < Formula
   desc "hyperconverged build tool to build projects without pre-requisites"
   homepage "https://github.com/evo-cloud/hmake"
-  url "https://github.com/evo-cloud/hmake/releases/download/v1.0.0rc2/hmake-1.0.0rc2-darwin-amd64.tar.gz"
-  version "1.0.0rc2"
-  sha256 "18c52de31dac5400f3e6decb2f70017445d378886ab7d177cf7afac1ebc20ede"
+  url "https://github.com/evo-cloud/hmake/releases/download/v1.0.0rc3/hmake-1.0.0rc3-darwin-amd64.tar.gz"
+  version "1.0.0rc3"
+  sha256 "8154eeaeb6878c982cfa0c5de279a05c50a0dcc52e7a99c894b896a100bf7c79"
 
   def install
     bin.install "hmake"
