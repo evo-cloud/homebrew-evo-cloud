49747993224dce85cef4820e87810871d1c5a945
EaseWay Hu <easeway@gmail.com>
2016-08-31 23:17:47 -0700

    release hmake v1.1.0

--- Formula/hmake.rb
+++ Formula/hmake.rb
@@ -1,9 +1,9 @@
 class  Hmake < Formula
   desc "hyperconverged build tool to build projects without pre-requisites"
   homepage "https://evo-cloud.github.io/hmake"
-  url "https://github.com/evo-cloud/hmake/releases/download/v1.1.0rc4/hmake-darwin-amd64.tar.gz"
-  version "1.1.0rc4"
-  sha256 "d545f27856540d8568bb20224a6575cbc5e5abad1caf18c1285259f49ed0e553"
+  url "https://github.com/evo-cloud/hmake/releases/download/v1.1.0/hmake-darwin-amd64.tar.gz"
+  version "1.1.0"
+  sha256 "e12d6ae10fbb5456465673e3d7418c09c28e31dc07fcc3692da578b00e312977"
 
   def install
     bin.install "hmake"
